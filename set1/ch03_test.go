2454400f263be65e9d16c64aa80d5e2c168a0f24
Annis Souames <souames.annis@gmail.com>
2026-07-08 21:56:27 +0200

    Improved challenge 1.3: english scoring and max index helpers

--- set1/ch03_test.go
+++ set1/ch03_test.go
@@ -3,23 +3,27 @@ package set1
 import (
 	"encoding/hex"
 	"testing"
+
 	"github.com/annis-souames/cryptopals/internal/cryptutil"
 )
 
-
 func TestCh03(t *testing.T) {
 	input := "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-	data,_ := hex.DecodeString(input)
+	data, _ := hex.DecodeString(input)
+	var scores [256]int
 	// We all try all combination of a 1 byte key: 256 possibilities (0-255)
 	for key := 0; key < 256; key++ {
 		got := cryptutil.SingleByteXOR(data, byte(key))
+		scores[key] = scoreEnglishLike(got)
 		// Here we need to check if got is english-like
 		// For simplicity, we will just print the result for now
-		t.Logf("Key: %d, Result: %s, Score: %d", key, got, scoreEnglishLike(got))
+		t.Logf("Key: %d, Result: %s, Score: %d", key, got, scores[key])
 	}
+
+	keyMaxScore := maxIdx(scores[:])
+
+	t.Logf("The key with highest score is %d and the original input is %s", keyMaxScore, cryptutil.SingleByteXOR(data, byte(keyMaxScore)))
 }
-
-
 
 // This function scores how "English-like" a byte slice is.
 func scoreEnglishLike(data []byte) int {
@@ -32,5 +36,18 @@ func scoreEnglishLike(data []byte) int {
 			score += 1
 		}
 	}
-	return score	
+	return score
+}
+
+// helper func to index of max in a slice
+func maxIdx(slice []int) int {
+	max := 0
+	maxIdx := 0
+	for i, v := range slice {
+		if v > max {
+			max = v
+			maxIdx = i
+		}
+	}
+	return maxIdx
 }
